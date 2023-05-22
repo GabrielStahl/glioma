f5fc45ee783f55735b28949faa9f335b6cfd62a2
Gabriel Stahl <gabriel.stahl@web.de>
2023-05-22 10:06:51 +0100

    current update

--- feature_extraction_cluster.py
+++ feature_extraction_cluster.py
@@ -10,14 +10,16 @@ import traceback
 from scipy.stats import pearsonr
 import numpy as np
 
+# Configure Settings
+dataset = "training" # Choose from: ["training", "validation", "test"] -- this will determine which dataset to use
+# Filter = "Square" # Choose from: ["Original", "SquareRoot", "Wavelet", "Square", "Exponential", "Logarithm"]
+Filter = sys.argv[1] # Pass the filter as an argument in the bash file
+Store_Original = False # Choose from: [True, False] 
+# choose modalities from: ["T1","T2","DTI_eddy_FA", "FLAIR", "SWI", "DWI", "T1_contrast", "DTI_eddy_MD", "ADC", "ASL"]
+acquisitions = ["T1","T2","DTI_eddy_FA", "FLAIR", "SWI", "DWI", "T1_contrast", "DTI_eddy_MD", "ADC", "ASL"] 
+
 def get_settings():
     environment = os.environ.get('PYRADIOMICS_ENV', 'local')  # Default to 'local' if the environment variable is not set
-
-    dataset = "validation" # Choose from: ["train", "validation", "test"] -- this will determine which dataset to use
-    Filter = "SquareRoot" # Choose from: ["Original", "SquareRoot"]
-    
-    # choose modalities from: ["T1","T2","DTI_eddy_FA", "FLAIR", "SWI", "DWI", "T1_contrast", "DTI_eddy_MD", "ADC", "ASL"]
-    acquisitions = ["T1","T2","DTI_eddy_FA", "FLAIR", "SWI", "DWI", "T1_contrast", "DTI_eddy_MD", "ADC", "ASL"] 
 
     if environment == 'local':
         base_dir = "/Users/Gabriel/Desktop/MSc_Dissertation/pyRadiomics/Data/"
@@ -30,7 +32,7 @@ def get_settings():
     else:
         raise ValueError("Unknown environment: please set PYRADIOMICS_ENV to 'local' or 'cluster'")
 
-    return base_dir, params, train_df, environment, dataset, Filter, acquisitions
+    return base_dir, params, train_df, environment
 
 def read_image_and_label(image_path, label_path, patient_id):
     Image = sitk.ReadImage(image_path)
@@ -54,15 +56,26 @@ def extract_features(params, Image, Label, patient_id):
         return extractor, result
     
 
-def apply_filters_on_images(extractor, Filter):
+def apply_filters_on_images(extractor):
 
     # By default, only 'Original' (no filter applied) is enabled. Optionally enable some image types:
-    if Filter == "SquareRoot":
-        extractor.enableImageTypeByName('SquareRoot')
 
+    filter_types = {
+        "SquareRoot": 'SquareRoot',
+        "Wavelet": 'Wavelet',
+        "Square": 'Square',
+        "Exponential": 'Exponential',
+        "Logarithm": 'Logarithm'
+    }
+
+    for filter_type, filter_name in filter_types.items():
+        if Filter == filter_type:
+            extractor.enableImageTypeByName(filter_name)
+    
     print('Filters applied on the image:')
     for imageType in extractor.enabledImagetypes.keys():
         print('\t' + imageType)
+
     return extractor
 
 def calculate_features(extractor, Image, Label):
@@ -107,13 +120,19 @@ def fetch_data(base_dir, patient_id, img_type):
     return image_path, label_path
     
 def main():
-    base_dir, params, train_df, environment, dataset, Filter, acquisitions = get_settings()
+    base_dir, params, train_df, environment = get_settings()
     lost_patients = []
     
     for cImg in acquisitions:
         img_type = cImg 
+        
         feature_data = pd.DataFrame() # Initialize feature data df
         feature_data_sqrroot = pd.DataFrame() # Initialize feature data df
+        feature_data_wavelet = pd.DataFrame() # Initialize feature data df
+        feature_data_square = pd.DataFrame() # Initialize feature data df
+        feature_data_exponential = pd.DataFrame() # Initialize feature data df
+        feature_data_logarithm = pd.DataFrame() # Initialize feature data df
+
         errors_id = pd.DataFrame()
         count = 0
 
@@ -125,16 +144,34 @@ def main():
             try:
                 Image, Label = read_image_and_label(image_path, label_path, patient_id)
                 extractor, result = extract_features(params, Image, Label, patient_id) # Here it goes wrong if label < 3
-                extractor = apply_filters_on_images(extractor, Filter)
+                extractor = apply_filters_on_images(extractor)
                 featureVector = calculate_features(extractor, Image, Label)
                 
+                # Always store the original features in the feature_data DataFrame
                 original_features = {key: value for key, value in featureVector.items() if key.startswith('original_firstorder_')}
                 feature_data = feature_data.append(pd.DataFrame(original_features, index=[patient_id]))
 
+                # Additionally, store the filtered features in separate DataFrames
                 if Filter == "SquareRoot":
                     sqrroot_features = {key: value for key, value in featureVector.items() if key.startswith('squareroot_firstorder_')}
                     feature_data_sqrroot = feature_data_sqrroot.append(pd.DataFrame(sqrroot_features, index=[patient_id]))
 
+                if Filter == "Wavelet":
+                    wavelet_features = {key: value for key, value in featureVector.items() if key.startswith('wavelet-LLH_firstorder_')}
+                    feature_data_wavelet = feature_data_wavelet.append(pd.DataFrame(wavelet_features, index=[patient_id]))
+
+                if Filter == "Square":
+                    square_features = {key: value for key, value in featureVector.items() if key.startswith('square_firstorder_')}
+                    feature_data_square = feature_data_square.append(pd.DataFrame(square_features, index=[patient_id]))
+
+                if Filter == "Exponential":
+                    exponential_features = {key: value for key, value in featureVector.items() if key.startswith('exponential_firstorder_')}
+                    feature_data_exponential = feature_data_exponential.append(pd.DataFrame(exponential_features, index=[patient_id]))
+
+                if Filter == "Logarithm":
+                    logarithm_features = {key: value for key, value in featureVector.items() if key.startswith('logarithm_firstorder_')}
+                    feature_data_logarithm = feature_data_logarithm.append(pd.DataFrame(logarithm_features, index=[patient_id]))
+    
             except Exception as e:
                 count += 1
                 tb_str = traceback.format_exception(etype=type(e), value=e, tb=e.__traceback__)
@@ -143,19 +180,26 @@ def main():
                 lost_patients.append(patient_id)
 
         print(f'Number of errors occurred: {count} --> patients affected by error {set(lost_patients)}')
-         # Save the feature_data DataFrame as a CSV file in the base_dir
-        feature_data.to_csv(os.path.join(base_dir, f"{dataset}_firstorder_original_{img_type}.csv"))    
+        
+        # Save the feature_data DataFrame as a CSV file in the base_dir
+        if Store_Original == True:
+            feature_data.to_csv(os.path.join(base_dir, f"{dataset}_firstorder_original_{img_type}.csv"))    
 
         if Filter == "SquareRoot":
             feature_data_sqrroot.to_csv(os.path.join(base_dir, f"{dataset}_firstorder_sqrroot_{img_type}.csv"))
 
-
+        if Filter == "Wavelet":
+            feature_data_wavelet.to_csv(os.path.join(base_dir, f"{dataset}_firstorder_wavelet_{img_type}.csv"))
+        
+        if Filter == "Square":
+            feature_data_square.to_csv(os.path.join(base_dir, f"{dataset}_firstorder_square_{img_type}.csv"))
+        
+        if Filter == "Exponential":
+            feature_data_exponential.to_csv(os.path.join(base_dir, f"{dataset}_firstorder_exponential_{img_type}.csv"))
+        
+        if Filter == "Logarithm":
+            feature_data_logarithm.to_csv(os.path.join(base_dir, f"{dataset}_firstorder_logarithm_{img_type}.csv"))
             
+        
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
